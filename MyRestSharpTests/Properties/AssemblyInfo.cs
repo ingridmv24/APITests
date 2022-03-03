319bea60b806b4e69bb200237a52edfe9141e73a
ingridmv <ingridmunera24@gmail.com>
2022-03-03 12:15:20 -0500

    API RestSharp Tests

---
+++ MyRestSharpTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyRestSharpTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyRestSharpTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c858ed83-f3a4-4b4d-8879-ce0adc7b7664")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyRestSharpTests/Test/TestClass.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+
+namespace MyRestSharpTests.Test
+{
+    [TestClass]
+    public class TestClass
+    {
+        string uri = "https://reqres.in";
+
+        [TestMethod]
+        public void Test_1_ListUsers()
+        {
+            RestClient client = new RestClient(uri);
+
+            RestRequest request = new RestRequest("api/users?page=1", Method.Get);
+
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            Console.WriteLine(response.Content);
+
+            Assert.AreEqual(200,(int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public void Test_2_SpecificUser()
+        {
+            RestClient client = new RestClient(uri);
+
+            var apiUser = 2;
+
+            RestRequest request = new RestRequest($"api/users/{apiUser}", Method.Get);
+
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            Console.WriteLine(response.Content);
+
+            Assert.AreEqual(200,(int)response.StatusCode);
+        }
+
+        [TestMethod]
+        public void Test_3_CreateUser()
+        {
+            RestClient client = new RestClient(uri);
+
+            RestRequest request = new RestRequest("api/users",Method.Post);
+
+            var body = new
+            {
+                name = "morpheus",
+                job = "leader"
+            };
+
+            request.AddJsonBody(body);
+
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            Console.WriteLine(response.Content);
+            Assert.AreEqual(201,(int)response.StatusCode);
+            ;
+
+        }
+
+        [TestMethod]
+        public void Test_4_DeleteUser()
+        {
+            RestClient client = new RestClient(uri);
+
+            var aPiUserDelete = 2;
+
+            RestRequest request = new RestRequest($"/api/users/{aPiUserDelete}", Method.Delete);
+
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            Console.WriteLine(response.Content);
+
+            Assert.AreEqual(204,(int)response.StatusCode);
+                 
+        }
+
+        [TestMethod]
+        public void Test_5_RegisterService()
+        {
+            RestClient client = new RestClient(uri);
+
+            RestRequest request = new RestRequest("api/register",Method.Post);
+
+            var body = new
+            {
+                email = "sydney@fife"
+            };
+
+            request.AddJsonBody(body);
+
+            RestResponse response = client.ExecuteAsync(request).Result;
+
+            Console.WriteLine(response.Content);
+            Assert.AreEqual(400, (int)response.StatusCode);
+        }
+    }   
+}
